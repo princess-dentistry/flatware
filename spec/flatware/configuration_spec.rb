4eef8ae22ab68b25ba3b1449b27590acf4de4a14
Chris Irish <irish@velocitylabs.io>
2026-04-02 13:26:51 -0700

    add test for after_forks_complete hook

--- spec/flatware/configuration_spec.rb
+++ spec/flatware/configuration_spec.rb
@@ -8,6 +8,20 @@ describe Flatware::Configuration do
     expect do
       Flatware.configuration.before_fork.call
       Flatware.configuration.after_fork.call(nil)
+      Flatware.configuration.after_forks_complete.call
     end.to_not raise_error
+  end
+
+  describe '#after_forks_complete' do
+    after { Flatware.configuration.reset! }
+
+    it 'stores and returns the given block' do
+      called = false
+      Flatware.configure do |config|
+        config.after_forks_complete { called = true }
+      end
+      Flatware.configuration.after_forks_complete.call
+      expect(called).to be true
+    end
   end
 end
